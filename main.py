eed12498b01516c2692f7896ee20310cdfd81223
Jahid Ali <72189635+LonePythonista@users.noreply.github.com>
2020-10-01 13:58:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ print('Hello world')
 
 print('I am a good boy')
 # Added one comment
-n1=int(input('Enter first number'))
-n2=int(input('Enter the second number'))
-print(n1+n2)
+def add():
+  n1=int(input('Enter first number'))
+  n2=int(input('Enter the second number'))
+  print(n1+n2)
+add()
